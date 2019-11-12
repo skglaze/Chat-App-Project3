d9820490462991ead46a297a7ff410f6c43db930
Sam Glaze <skglaze@gmail.com>
2019-11-12 15:25:10 -0500

    added socket io to server and made changes to room component

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import io from 'socket.io-client'
+
+let socket = io('http://localhost:4000/')
+
+let socketSendChat = (value) => {
+
+}
 
 export default class Room extends Component {
     state = {
-        room: {}
+        room: {},
+        messages: []
     }
 
     componentDidMount() {
@@ -12,6 +20,19 @@ export default class Room extends Component {
                 this.setState({ room: response.data[0] })
                 console.log(this.state.room)
             })
+        axios.get(`/api/messages/messages/${this.props.match.params.roomId}`)
+            .then((response) => {
+                this.setState({ messages: response.data })
+                console.log(this.state.messages)
+            })
+        socket.on('new-message', (event) => {
+            if (this.props.match.params.id !== event.data.roomId) {
+                return
+            }
+            const previousState = { ...this.state }
+            previousState.messages.push(event.data)
+            this.setState(previousState)
+        })
     }
 
     render() {
@@ -20,6 +41,7 @@ export default class Room extends Component {
                 <h1>Hey Bois</h1>
 
                 {this.state.room.name}
+                {this.state.messages}
             </div>
         )
     }
--- controllers/message.js
+++ controllers/message.js
@@ -3,31 +3,31 @@ const messageRouter = express.Router()
 
 const messageApi = require('../models/message.js')
 
-messageRouter.get('/', (req, res) => {
+messageRouter.get('/messages/', (req, res) => {
     messageApi.getAllMessages().then((messages) => {
         res.json(messages)
     })
 })
 
-messageRouter.get('/:messageId', (req, res) => {
+messageRouter.get('/messages/:messageId', (req, res) => {
     messageApi.getOneMessage(req.params.messageId).then((message) => {
         res.json(message)
     })
 })
 
-messageRouter.post('/', (req, res) => {
+messageRouter.post('/messages/', (req, res) => {
     messageApi.addNewMessage(req.body).then((newMessage) => {
         res.json(newMessage)
     })
 })
 
-messageRouter.put('/:messageId', (req, res) => {
+messageRouter.put('/messages/:messageId', (req, res) => {
     messageApi.updateMessage(req.params.messageId, req.body).then((updatedMessage) => {
         res.json(updatedMessage)
     })
 })
 
-messageRouter.delete('/:messageId', (req, res) => {
+messageRouter.delete('/messages/:messageId', (req, res) => {
     messageApi.deleteMessage(req.params.messageId)
         .then((deletedMessage) => {
             res.json(deletedMessage)
